e830278a54841073e0e09643b09b3178c2ab52fc
padailey <peteradailey@gmail.com>
2016-01-11 12:17:54 -0800

    Initial Commit.

---
+++ RotateLogs/rotateLogs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Date/Timestamp format
+TODAY=`date '+%Y-%m-%d'`;
+
+# Source and Destination directories
+# Note lack of trailing / makes output easier to discern
+src=/Users/peterdailey/Test
+dst=~/Desktop
+
+# Copy files in source directory
+# TODO After testing, change cp to mv
+# TODO Save to USB and /log directory
+for f in $src/*.csv
+do
+        filename=$(basename "$f")
+        extension="${filename##*.}"
+        filename="${filename%.*}"
+
+        out=$filename-$TODAY.$extension
+        echo "Processing: $filename"
+        echo "Renaming and moving to desktop: $out"
+        # Copy to the destination
+        cp $f $dst/$out
+done
